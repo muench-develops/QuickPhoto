8eda974bfd1c21230a2ea45b9fb4adcf7bba2e55
muench-develops <justin.muench@iubh-fernstudium.de>
2021-03-16 21:16:05 +0100

    Login.html added Präsentation erweitert

---
+++ Code/assets/js/login.js
@@ -0,0 +1,7 @@
+$(document).ready(function () {
+  $('#btnSubmit').click(function () {
+    alert($("#user").val());
+    alert($('#password').val());
+    //$('#btnSubmit').show();
+  });
+});
--- Code/assets/js/main.js
+++ Code/assets/js/main.js
@@ -1,65 +1,35 @@
-var btnAbonnieren = document.getElementById("btnAbonnieren");
-var divAbonniert = document.getElementById("divAbonniert");
-var Suche = document.getElementById("Suche");
-var Galerie = document.getElementById("Galerie");
-var Bilder = Galerie.getElementsByTagName("img");
+$(document).ready(function () {
+  //Ein und Ausblenden von Abonniert Btn
+  $('#btnAbonnieren').click(function () {
+    $('#btnAbonnieren').hide();
+    $('#divAbonniert').show();
+  });
+  $('#divAbonniert').click(function () {
+    $('#btnAbonnieren').show();
+    $('#divAbonniert').hide();
+  });
 
-btnAbonnieren.addEventListener("click", function (event) {
-    event.preventDefault();
-    if (btnAbonnieren.style.display === "none") {
-        btnAbonnieren.style.display = "block";
-        divAbonniert.style.display = "none";
-    } else {
-        btnAbonnieren.style.display = "none";
-        divAbonniert.style.display = "block";
-    }
+  //Image like Funktion
+  $('#Galerie img').each(function () {
+    $(this).on('click', function () {
+      if (!$(this).hasClass('border border-primary')) {
+        $(this).addClass('border border-primary');
+      } else {
+        $(this).removeClass('border border-primary');
+      }
+    });
+  });
+
+  //Suche - Funktion
+  $('#Suche').on('input', function () {
+    var suchwert = $(this).val();
+    console.log(suchwert);
+    $('#Galerie img').each(function () {
+      if ($(this).attr('src').indexOf(suchwert) != -1) {
+        $(this).show();
+      } else {
+        $(this).hide();
+      }
+    });
+  });
 });
-
-function toggleAbonniert() {
-    if (divAbonniert.style.display === "block") {
-        btnAbonnieren.style.display = "block";
-        divAbonniert.style.display = "none";
-    }
-}
-
-Suche.addEventListener("change", function (event) {
-    var suchWert = new String(event.target.value);
-    console.log(suchWert);
-    console.log("Suche gestartet");
-    for (var i = 0; i < Bilder.length; i++) {
-        if (suchWert.length >= 1) {
-            if (Bilder[i].src.includes(suchWert)) {
-                Bilder[i].style.display = "block";
-            } else {
-                Bilder[i].style.display = "none";
-            }
-        } else {
-            Bilder[i].style.display = "block";
-        }
-    }
-});
-
-document.addEventListener("DOMContentLoaded", (event) => {
-    console.log("DOM is ready.");
-    for (var i = 0; i < Bilder.length; i++) {
-        if (Bilder[i].onclick == null) {
-            Bilder[i].addEventListener(
-                "click",
-                imgClick.bind(Bilder[i]),
-                false
-            );
-        }
-    }
-});
-
-// Funkt nicht xD
-function imgClick() {
-    console.log(this.classList);
-    if (this.classList.contains("border border-success")) {
-        this.classList.add("border border-success");
-        console.log(this.classList);
-    } else {
-        this.classList.remove("border");
-        this.classList.remove("border-success");
-    }
-}
